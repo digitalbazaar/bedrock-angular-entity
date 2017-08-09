5d6cfd79f07cfcdae97626b420c23d7017e6da86
Manu Sporny <msporny@digitalbazaar.com>
2017-08-09 14:03:12 -0400

    Add dependency for bedrock-angular-form.

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
  * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
  */
 import angular from 'angular';
-import * as bedrock from 'bedrock-angular';
 import EntityComponent from './entity-component';
 
 'use strict';
 
-const module = angular.module('bedrock.entity', []);
+const module = angular.module('bedrock.entity', ['bedrock.form']);
 module.component('brEntity', EntityComponent);
