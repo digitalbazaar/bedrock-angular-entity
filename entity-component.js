31518ef1441a53fa11aceafc0fc529826579ce6b
Manu Sporny <msporny@digitalbazaar.com>
2017-08-09 16:54:31 -0400

    Enable template loading support.

--- entity-component.js
+++ entity-component.js
@@ -4,6 +4,11 @@
 'use strict';
 
 export default {
+  bindings: {
+    schema: '<brEntitySchema',
+    template: '<?brEntityTemplate',
+    onChange: '<brOnChange'
+  },
   controller: Ctrl,
   templateUrl: 'bedrock-angular-entity/entity-component.html'
 };
@@ -11,4 +16,14 @@ export default {
 /* @ngInject */
 function Ctrl() {
   const self = this;
+
+  self.$onInit = () => {
+    self.entity = {};
+
+    if(!self.template) {
+      self.template = 'bedrock-angular-entity/single-column-layout.html';
+    }
+  };
+
+  console.log("EC GOT BINDINGS", self);
 }
