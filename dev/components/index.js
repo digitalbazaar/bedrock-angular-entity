9968f8b74d36b1e0b2646e54ca28dc02238a3126
Matthew Collier <collier.matthew@gmail.com>
2017-08-16 13:08:22 -0400

    Fix style.

--- dev/components/index.js
+++ dev/components/index.js
@@ -8,7 +8,7 @@ import * as bedrock from 'bedrock-angular';
 import HomeComponent from './home-component';
 
 const module = angular.module('bedrock-angular-entity-dev', [
- 'bedrock.entity', 'ngError', 'ngMaterial', 'ngMessages']);
+  'bedrock.entity', 'ngError', 'ngMaterial', 'ngMessages']);
 module.component('baetHome', HomeComponent);
 
 bedrock.setRootModule(module);
